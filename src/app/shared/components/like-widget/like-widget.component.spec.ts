dda0b3743a981a8d13388324aa3892a2be1368d5
Willian Menezes <willian.menezes@lambda3.com.br>
2021-06-17 07:57:39 -0300

    Utilizando o spyon

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -33,12 +33,10 @@ describe(LikeWidgetComponent.name, () => {
     expect(component.id).toBe(someId);
   });
 
-  it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called`, (done) => {
+  it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called`, () => {
+    spyOn(component.liked, 'emit');
     fixture.detectChanges();
-    component.liked.subscribe(() => {
-      expect(true).toBeTrue();
-      done();
-    });
     component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 });
